4bc55e47efdb0b3cf7ffeec88c874b7e1b61ecbd
tm <18730222889@126.com>
2016-07-12 16:00:03 +0800

    init

---
+++ phoneios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  phoneios
+//
+//  Created by tm on 16/7/12.
+//  Copyright © 2016年 tm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ phoneios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  phoneios
+//
+//  Created by tm on 16/7/12.
+//  Copyright © 2016年 tm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
